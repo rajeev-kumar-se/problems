e7065b3d9bbed6250ac6380c698ef68ddafed473
Rajeev Kumar <135874870+rajeev-kumar-se@users.noreply.github.com>
2023-10-31 12:50:13 +0530

    Update code.cpp

--- add-two/code.cpp
+++ add-two/code.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+void solve(){
+  int a, b;
+  cin>>a>>b;
+  cout<<(a+b)<<endl;
+  return;
+}
+int main() {
+     int t ; cin>>t;
+     while(t--)solve();
+     return 0;
+}
