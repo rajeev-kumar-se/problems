d4b70378132d3c84151d9e3333edbc1906753353
Rajeev Kumar <rajeev.kumar.se@gmail.com>
2023-11-01 18:50:42 +0530

    deleted:    add-two/desc.txt 	new file:   add-two/problem.sql 	new file:   gen.sh 	new file:   max-subarray-sum/in.txt 	new file:   max-subarray-sum/out.txt 	new file:   max-subarray-sum/problem.sql 	deleted:    repetitions/desc.txt 	new file:   repetitions/problem.sql 	renamed:    weird-algorithm/desc.txt -> weird-algorithm/problem.sql

---
+++ add-two/problem.sql
@@ -0,0 +1,10 @@
+INSERT INTO `problemsMetadata` (`problemId`, `problemTitle`, `problemDescription`, `problemInputDescription`, `problemOutputDescription`, `problemConstraints`, `problemSubtask`, `authorId`)
+VALUES
+    ('add-two-numbers', 
+    'Add Two Numbers', 
+    'Given two numbers a and b and t test cases, print the sum of two numbers.', 
+    'The input consists of t lines. Each line contains two integers a and b separated by a space.',
+    'For each test case, output the sum of a and b on a new line.',
+    '1 <= t <= 100, -10^9 <= a, b <= 10^9', 
+    'Subtask 1: 1 <= t <= 10, -100 <= a, b <= 100', 
+    'AUTHOR003');
---
+++ repetitions/problem.sql
@@ -0,0 +1,10 @@
+INSERT INTO `problemsMetadata` (`problemId`, `problemTitle`, `problemDescription`, `problemInputDescription`, `problemOutputDescription`, `problemConstraints`, `problemSubtask`, `authorId`)
+VALUES
+    ('longest-dna-repetition',
+    'Longest DNA Repetition',
+    'Given a DNA sequence, find the length of the longest repetition, i.e., the maximum-length substring containing only one type of character (A, C, G, or T).',
+    'The input consists of a single line containing a string of n characters.',
+    'Print one integer: the length of the longest repetition.',
+    '1 <= n <= 10^6',
+    'Subtask 1: 1 <= n <= 100',
+    'AUTHOR004');
