b2c5291dcd94666c379b5d2d45b173201cfd6c3d
Rajeev Kumar <rajeev.kumar.se@gmail.com>
2024-04-06 13:42:58 +0530

    modified:   generator.cc

--- generator.cc
+++ generator.cc
@@ -28,9 +28,6 @@ void dbg_out() { cerr << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
-// Debug 2
-template <typename T=ll> T rint(ll a, ll b) { return uniform_int_distribution<T>(a, b)(mt_rng);}
-vector<ll> rvec(int N,int l, int r) { vector<ll> v(N); generate(v.begin(), v.end(), [&](){ return rint<ll>(l, r); }); return v;}
 // Exponentiation
 ll pow(ll a, ll b, ll m = MOD) { a %= m;ll res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1;}return res;}
 // Mod
@@ -49,6 +46,108 @@ void seive(){}
 string PROBLEM = "$file_name";
 std::ofstream fout("./"+PROBLEM+".in");
 // ========WRITE TEST CASE HERE=============
+// Debug 2
+template <typename T=ll> T rint(ll a, ll b) { return uniform_int_distribution<T>(a, b)(mt_rng);}
+vector<ll> rvec(int N,int l, int r) { vector<ll> v(N); generate(v.begin(), v.end(), [&](){ return rint<ll>(l, r); }); return v;}
+
+// Generate a random tree with 'n' vertices
+vector<vector<int>> rTree(int n) {
+    vector<vector<int>> tree(n);
+    unordered_set<int> visited;
+    for (int i = 1; i < n; ++i) {
+        int parent = rint(0, i - 1);
+        tree[parent].push_back(i);
+        tree[i].push_back(parent);
+        visited.insert(parent);
+    }
+    return tree;
+}
+
+// Generate a random graph with 'n' vertices and 'm' edges
+vector<pair<int, int>> rGraph(int n, int m, bool cyclic, bool directed) {
+    vector<pair<int, int>> edges;
+    if (!cyclic && m > (n * (n - 1)) / 2) {
+        cout << "Error: Too many edges requested for an acyclic graph with " << n << " vertices!" << endl;
+        return edges;
+    }
+    if (cyclic && m > n * n) {
+        cout << "Error: Too many edges requested for a cyclic graph with " << n << " vertices!" << endl;
+        return edges;
+    }
+    unordered_set<int> visited;
+    while (edges.size() < m) {
+        int u = rint(0, n - 1);
+        int v = rint(0, n - 1);
+        if (!cyclic && u == v) continue; // Skip self loops in acyclic graph
+        if (u != v && visited.find(u * n + v) == visited.end()) {
+            edges.push_back({u, v});
+            visited.insert(u * n + v);
+            if (!directed)
+                visited.insert(v * n + u);
+        }
+    }
+    return edges;
+}
+
+// Print the given tree
+void pTree(const vector<vector<int>>& tree) {
+    cout << "Tree:\n";
+    cout << "Number of vertices: " << tree.size() << endl;
+    int edges = 0;
+    for (int i = 0; i < tree.size(); ++i) {
+        edges += tree[i].size();
+    }
+    cout << "Number of edges: " << edges / 2 << endl;
+    for (int i = 0; i < tree.size(); ++i) {
+        cout << "Node " << i << " connected to:";
+        for (int neighbor : tree[i]) {
+            cout << " " << neighbor;
+        }
+        cout << endl;
+    }
+}
+
+// Print the given graph
+void pGraph(const vector<pair<int, int>>& graph) {
+    cout << "Graph:\n";
+    unordered_set<int> vertices;
+    for (auto [u, v] : graph) {
+        vertices.insert(u);
+        vertices.insert(v);
+    }
+    cout << "Number of vertices: " << vertices.size() << endl;
+    cout << "Number of edges: " << graph.size() << endl;
+    for (auto [u, v] : graph) {
+        cout << "Edge: " << u << " - " << v << endl;
+    }
+}
+
+// Generate random left-right queries from a given vector
+template <typename T>
+vector<pair<T, T>> rLR(const vector<T>& v) {
+    vector<pair<T, T>> queries;
+    random_device rd;
+    mt19937 mt_rng(rd());
+    
+    // Generate queries
+    for (size_t i = 0; i < v.size(); ++i) {
+        uniform_int_distribution<size_t> dist(i, v.size() - 1);
+        size_t l = dist(mt_rng);
+        size_t r = dist(mt_rng);
+        if (l > r) swap(l, r);
+        queries.push_back({v[l], v[r]});
+    }
+    return queries;
+}
+
+// Print left-right queries
+template <typename T>
+void pLR(const vector<pair<T, T>>& queries) {
+    cout << "Left-Right Queries:\n";
+    for (const auto& query : queries) {
+        cout << "(" << query.first << ", " << query.second << ")\n";
+    }
+}
 
 void writer(){
     // use: fout
